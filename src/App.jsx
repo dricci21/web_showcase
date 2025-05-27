dad1a2a6e451b7d626ec51acbeb79ea8b6b33156
dricci21 <dricci@umassd.edu>
2025-05-27 14:12:43 -0400

    push3

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-// App.jsx
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import QRCode from './pages/QRCode';
-import './App.css'; // Import the CSS file
-
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <nav className="navbar">
-        <Link to="/" className="nav-link">Home</Link>
-        <Link to="/about" className="nav-link">About Us</Link>
-        <Link to="/qrcode" className="nav-link">QR Code</Link>
-      </nav>
+    
+      <div>
+        <nav className="navbar">
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/about" className="nav-link">About Us</Link>
+          <Link to="/qrcode" className="nav-link">QR Code</Link>
+        </nav>
 
-      <div className="page-content">
-        <Routes>
-          <Route path="/web_showcase" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/qrcode" element={<QRCode />} />
-        </Routes>
+        <div className="page-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/qrcode" element={<QRCode />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    
   );
 }
 
 export default App;
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+const basename = import.meta.env.DEV ? '/' : '/web_showcase';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
